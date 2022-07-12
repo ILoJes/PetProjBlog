8c8f2b0197e28da835708da32ace3521e971c5f8
ILoJes <ilojes86@gmail.com>
2022-07-13 00:33:00 +0300

    first table

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,20 +2,23 @@ from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import EmailPostForm
 
 # Create your views here.
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+# def post_list(request):
+#     posts = Post.published.all()
+#     return render(request, 'blog/post/list.html', {'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
-                            publish__month=month, publish__day=day)
-    return render(request,'blog/post/detail.html',{'post': post})
+                             publish__month=month, publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
+
 
 def post_list(request):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) # По 3 статьи на каждой странице.
+    paginator = Paginator(object_list, 3)  # По 3 статьи на каждой странице.
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -25,10 +28,27 @@ def post_list(request):
     except EmptyPage:
         # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
         posts = paginator.page(paginator.num_pages)
-    return render(request,'blog/post/list.html', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    # Получение статьи по идентификатору.
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        # Форма была отправлена на сохранение.
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Все поля формы прошли валидацию.
+            cd = form.cleaned_data
+            # ... Отправка электронной почты.
+        else:
+            form = EmailPostForm()
+        return render(request, 'blog/post/share.html',
+                      {'post': post, 'form': form})
